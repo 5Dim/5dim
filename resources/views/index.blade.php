c0a44b7c6a80eb296801ff95a6fc2baad4c9640e
unknown <odinspn@gmail.com>
2018-06-10 10:53:53 +0200

    jjjj666

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,92 +1,251 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <meta name="author" content="Quinta Dimensión" /> 
+        <meta name="revisit-after" content="7 days" /> 
+        <meta name="ROBOTS" content="INDEX, ALL, FOLLOW" /> 
+        <meta http-equiv="contents" content="Quinta Dimensión, 5 dimensión, 5dim, juego gratis, juego online, juego espacial" /> 
+        <meta name="description" content="Quinta Dimensión, juego espacial gratis online" /> 
+        <meta http-equiv="Keywords" content="Quinta Dimensión, 5 dimensión, 5dim, juego gratis, juego online, juego espacial" /> 
+        <meta content="Quinta Dimensión, juego espacial gratis online" /> 
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>5Dim - Quinta Dimension</title>
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+   <!-- MAIN -->
+		<div id="main">
+			<!-- wrapper -->
+			<div class="wrapper">
+				<!-- home-content -->
+				<div class="home-content">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+					<div class="shadow-divider"></div>
+					<!-- headline -->
+					<div class="headline"><?php echo $web12;
+					//echo " <a href=\"registro2.php\">  $web20 </a>";
+					?>
+                        <h5>Ronda 2015-2016 cerrada</h5>
+					</div>
+                    <!-- ENDS headline -->
+                    
 
-            .full-height {
-                height: 100vh;
-            }
+                    
+					
+					             <!-- slideshow -->
+					<div id="slideshow">
+						<!--<a href="#" id="prev"></a>
+	          			<a href="#" id="next"></a>-->
+						<a href="" id="slideshow-link" ><span></span></a>
+						<!-- slides -->
+						<ul id="slides">
+			                <li><img src="images/1.png"  alt="Imagen" /></li>
+			                <li><img src="images/2.png"  alt="Imagen" /></li>
+			                <li><img src="images/3.png"  alt="Imagen" /></li>
+                            <li><img src="images/4.png"  alt="Imagen" /></li>
+			          	</ul>
+			          	<!-- ENDS slides -->
+					</div>
+					<!-- ENDS slideshow -->
+					<!-- front-left-col -->
+					<div class="front-left-col">
+						<div class="bullet-title">
+							<div class="big"><?php echo $ntex1; ?></div>
+							<div class="small"><?php echo $ntex2; ?></div>
+						</div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <div id="example1"></div>
+    <script type="text/javascript" src="twitterFetcher_v10_min.js"></script>
+    <script>twitterFetcher.fetch('361564690305929216', 'example1', 3, true, false, true);</script>
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
+						<p><a href="https://twitter.com/5_Dim" class="link-button right" target="_blank"><span><?php echo $ntex3; ?></span></a></p>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">chupame un pie</a>
-  
-                </div>
-            </div>
+                        
+					</div>
+					<!-- ENDS front-left-col -->
+					
+                    
+					<!-- front-right-col-->
+					<div class="front-right-col">
+						<div class="bullet-title">
+							<div class="big"><?php echo $ntex4; ?></div>
+							<div class="small"><?php echo $ntex5; ?></div>
+						</div>
+						<ul class="blocks-holder">
+							<li class="block">
+								<div class="block-ribbon">
+									<div class="left">
+										<div class="block-title"><a href="#"><?php echo $ntex6; ?></a></div>
+										<div class="block-date"><?php echo $ntex7; ?></div>
+									</div>
+									<div class="right"></div>
+								</div>
+								<a href="eljuego/web2/instrucciones/arbol.php?nodjuego=1" target="_blank"><img src="img/dummies/home-block-1.jpg" alt="Thumb" class="thumb" title="Thumbnail" /> </a>
+							</li>
+							<li class="block">
+								<div class="block-ribbon">
+									<div class="left">
+										<div class="block-title"><a href="#"><?php echo $ntex8; ?></a></div>
+										<div class="block-date"><?php echo $ntex9; ?></div>
+									</div>
+									<div class="right"></div>
+								</div>
+								<a href="eljuego/web2/instrucciones/arbolcons.php?nodjuego=1" target="_blank" ><img src="img/dummies/home-block-2.jpg" alt="Thumb" class="thumb" title="Thumbnail" /> </a>
+							</li>
+							<li class="block">
+									<div class="block-ribbon">
+									<div class="left">
+										<div class="block-title"><a href="#"><?php echo $ntex10; ?></a></div>
+										<div class="block-date"><?php echo $ntex11; ?></div>
+									</div>
+									<div class="right"></div>
+								</div>
+								<a href="eljuego/web2/instrucciones/arbolinvest.php?nodjuego=1" target="_blank" ><img src="img/dummies/home-block-3.jpg" alt="Thumb" class="thumb" title="Thumbnail" /> </a>
+								
+							</li>
+							<li class="block">
+								<div class="block-ribbon">
+									<div class="left">
+										<div class="block-title"><a href="#"><?php echo $ntex12; ?></a></div>
+										<div class="block-date"><?php echo $ntex13; ?></div>
+									</div>
+									<div class="right"></div>
+								</div>
+								<a href="eljuego/web2/mapa10.php?nodjuego=1" ><img src="img/dummies/home-block-4.jpg" alt="Thumb" class="thumb" title="Thumbnail" /> </a>
+							</li>
+						</ul>
+                                               
+					</div>
+					<!-- ENDS front-left-col -->
+          
+<!-- POSTS -->
+      <div id="posts"> 
+        <!-- post -->
+        <div class="post"> 
+          <!-- post-header -->
+          <div class="bullet-title">
+            <div class="big"><?php echo $web13; ?></div>
+            <div class="small"><?php echo $web14; ?></div>
+          </div>
+          <!-- ENDS post-header --> 
+          <div class="excerpt">
+            <p><?php echo $web15; ?></p><br />
+            <p><?php echo $web16; ?></p><br />
+            <p><?php echo $web17; ?></p><br />
+            <p><?php echo $web18; ?></p><br />
+            <p><?php echo $web19; ?></p>
+          </div>
+         
         </div>
-    </body>
+        <!-- ENDS post --> 
+                
+      </div>
+      <!-- ENDS POSTS --> 
+      
+      <!-- sidebar -->
+      <ul id="sidebar">
+        
+        <!-- recent posts -->
+        <li class="recent-posts">
+          <h2 class="custom"><span><?php echo $cabezo1; ?></span></h2>
+          <ul>
+            <li> 
+              <div class="recent-excerpt ">
+                <div><a><?php echo $cara10; ?></a></div>
+                <div><?php echo $cara11; ?></div>
+              </div>
+            </li>
+            <li> 
+              <div class="recent-excerpt">
+                <div><a><?php echo $cabezo2; ?></a></div>
+                <div><?php echo $cara20; ?></div>
+              </div>
+            </li>
+            <li> 
+              <div class="recent-excerpt">
+                <div><a><?php echo $cara21; ?></a></div>
+              </div>
+            </li>
+            <li> 
+              <div class="recent-excerpt">
+                <div><a><?php echo $cabezo3; ?></a></div>
+                <div><?php echo $cara30; ?></div>
+              </div>
+            </li>
+            <li> 
+              <div class="recent-excerpt">
+                <div><a><?php echo $cabezo4; ?></a></div>
+                <div><?php echo $cara40; ?></div>
+              </div>
+            </li>  
+            <li> 
+              <div class="recent-excerpt">
+                <div><a><?php echo $cabezo5; ?></a></div>
+              </div>
+            </li>                      
+          </ul>
+        </li>
+        <!-- ENDS recent posts -->
+      </ul>
+      <!-- ENDS sidebar --> 
+                    
+				</div>
+   				
+                <div id="foro">    
+                    <div class="bullet-title">
+                    <div class="big">Actividad del foro</div>
+                    <div class="small">El mejor sitio para estar informado</div>
+                </div>
+			   <div id="mensajes"></div>
+               </div>
+			  <!-- ENDS home-content -->
+				<div class="clear"></div>
+				
+				
+		
+				
+			</div>
+			<!-- ENDS main-wrapper -->
+			
+		
+		</div>		
+		<!-- ENDS MAIN -->	
+		
+		<!-- FOOTER -->
+		<div id="footer">
+		<?php include "footer.php"; ?>
+		</div>
+		<!-- ENDS FOOTER -->
+
+
+		<!-- BOTTOM -->
+		<div id="bottom">
+		</div>
+		<!-- ENDS BOTTOM -->
+
+		<!-- start cufon -->
+		<script type="text/javascript"> 
+		
+		Cufon.now(); 
+        
+		autolog=0;
+<?php echo $varjava; ?>
+
+
+
+if (autolog>0) {
+document.form1.nick.value=user;
+document.form1.pass.value=pas;
+document.form1.submit();
+	
+};
+
+        </script>
+		<!-- ENDS start cufon -->
+	
+	</body>
 </html>
