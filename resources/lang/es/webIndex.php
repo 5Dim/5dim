ea1fe22924493e432518ec758e53094dcea68c99
odinspain <odinspn@gmail.com>
2018-06-16 20:02:37 +0200

    matame camion

--- resources/lang/es/webIndex.php
+++ resources/lang/es/webIndex.php
@@ -1,6 +1,8 @@
 <?php
 return [
 
+    /// poner <html lang="{{ app()->getLocale() }}">   @lang('webIndex.ntex4')
+
     'web1' => 'INICIO',
 'web2' => 'NOTICIAS',
 'web3' => 'CARACTERISTICAS',
--- resources/views/cabezo.blade.php
+++ resources/views/cabezo.blade.php
@@ -42,8 +42,8 @@
                     <!-- navigation -->
                     <div id=\"nav-holder\">
                         <ul id=\"nav\" class=\"sf-menu\">
-                            <li ><a href=\"http://5dim.es/\">  @lang('webIndex.web1') </a></li>
-                            <li ><a> @lang('webIndex.ntex14') </a>
+                            <li class="current_page_item"><a href=\"http://5dim.es/\">  @lang('webIndex.web1') </a></li>
+                            <li class="current_page_item"><a> @lang('webIndex.ntex14') </a>
                                 <ul>
                                     <li ><a href=\"astrometria.php?elidioma=elidioma\"> @lang('webIndex.cara6') </a></li>
                                     <li ><a href=\"economia.php?elidioma=elidioma\"> @lang('webIndex.cara5') </a></li>
@@ -52,9 +52,9 @@
                                     <li ><a href=\"politica.php?elidioma=elidioma\">  @lang('webIndex.cara3')  </a></li>
                                 </ul>
                             </li>
-                            <li ><a href=\"gallery.php?elidioma=elidioma\">  @lang('webIndex.ntex15') </a></li>
-                            <li ><a href=\"http://quintadim.foroactivo.com/\" target=\"_blank\" > @lang('webIndex.web6') </a></li>
-                            <li ><a href=\"contact.php?elidioma=elidioma\"> @lang('webIndex.web7') </a></li>
+                            <li class="current_page_item"><a href=\"gallery.php?elidioma=elidioma\">  @lang('webIndex.ntex15') </a></li>
+                            <li class="current_page_item"><a href=\"http://quintadim.foroactivo.com/\" target=\"_blank\" > @lang('webIndex.web6') </a></li>
+                            <li class="current_page_item"><a href=\"contact.php?elidioma=elidioma\"> @lang('webIndex.web7') </a></li>
                             
                             
                         </ul>
