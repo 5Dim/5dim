6a97fe8211d7db532a220e9b4ca8fb43e8ec11e0
odinspain <odinspn@gmail.com>
2018-06-19 23:55:55 +0200

    menu de recursos en html

--- resources/views/eljuego/recursosFrame.blade.php
+++ resources/views/eljuego/recursosFrame.blade.php
@@ -15,7 +15,7 @@
        <div class="col-5 text-center"> <div class="row"> 
             <div  id="" class="col-6"></div>             
             <div  id="flotasx" class="col-1">ataque</div>   
-            <div id="mensjx" class="col-1">mensaje</div>
+            <div id="" class="col-1"><a href="mensajes.php" target="_self"><img  src="{{ asset('img/elJuego/skin0/icons/ico-barra-men.png') }}" title="Mensajes"/></a></div>
             <div id="" class="col-1"><a href="misiones.php?tipo=1" target="_self"><img  src="{{ asset('img/elJuego/skin0/icons/ico-barra-mis.png') }}" title="Misiones"/></a></div>
             <div id="" class="col-1"><a href="estadisticas.php" target="_self"><img src="{{ asset('img/elJuego/skin0/icons/ico-barra-est.png') }}"  title="Estadisticas"/></a></div>
             <div id="" class="col-1"><a  href="http://es.5dim.wikia.com/wiki/Wiki_5dim" target="_blank"><img src="{{ asset('img/elJuego/skin0/icons/ico-barra-wik.png') }}"  title="Wikia"/></a></div>
@@ -134,33 +134,33 @@
         <div id="" class="col"><a id="constr" href="construccion.php" title="Construye tu imperio" target="_self">
                 <img title="Construcción"
                 src="{{ asset('img/elJuego/skin0/icons/ico-cons0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-cons1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-cons0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-cons1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-cons0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="hangar.php" target="_self">
                 <img title="Producción"
                 src="{{ asset('img/elJuego/skin0/icons/ico-prod0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-prod1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-prod0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-prod1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-prod0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="planeta3.php"  target="_self"> 
                 <img title="Planeta"
                 src="{{ asset('img/elJuego/skin0/icons/ico-pla0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-pla1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-pla0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-pla1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-pla0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="investigacion.php"  target="_self"> 
                 <img title="Investigación"
                 src="{{ asset('img/elJuego/skin0/icons/ico-inv0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-inv1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-inv0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-inv1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-inv0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="defensas.php" target="_self"> 
                 <img title="Defensa"
                 src="{{ asset('img/elJuego/skin0/icons/ico-def0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-def1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-def0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-def1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-def0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="diseno.php"  target="_self"> 
                 <img title="Diseños"
                 src="{{ asset('img/elJuego/skin0/icons/ico-dis0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-dis1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-dis0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-dis1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-dis0.png') }}" /></a></div>
 
         <div id="" class="col"><div id="m1" class="EstColorido1b" align="center">Selección de planeta<form action="menu0.php" method="post" name="sellectpla" target="_self" id="sellectpla">
                 <input type=hidden name="otrojugador" VALUE="1">
@@ -172,33 +172,33 @@
         <div id="" class="col"><a id="constr" href="mapa10.php"  target="_blank"> 
                 <img title="Astrometría"
                 src="{{ asset('img/elJuego/skin0/icons/ico-ast0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-ast1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-ast0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-ast1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-ast0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="flota.php?nmenu=2"  target="_self"> 
                 <img title="Flotas"
                 src="{{ asset('img/elJuego/skin0/icons/ico-flo0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-flo1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-flo0.png') }}" /></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-flo1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-flo0.png') }}" /></div>
         <div id="" class="col"><a id="constr" href="banco1.php"  target="_self"> 
                 <img title="Banco"
                 src="{{ asset('img/elJuego/skin0/icons/ico-ban0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-ban1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-ban0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-ban1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-ban0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="comercio13.php"  target="_self"> 
                 <img title="Comercio"
                 src="{{ asset('img/elJuego/skin0/icons/ico-com0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-com1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-com0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-com1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-com0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="alianza0.php"  target="_self"> 
                 <img title="Alianza"
                 src="{{ asset('img/elJuego/skin0/icons/ico-ali0.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-ali1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-ali0.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-ali1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-ali0.png') }}" /></a></div>
         <div id="" class="col"><a id="constr" href="ppal.php"  target="_self"> 
                 <img title="General"
                 src="{{ asset('img/elJuego/skin0/icons/ico-gen1.png') }}"    
-                onmouseover="this.src="{{ asset('img/elJuego/skin0/icons/ico-gen1.png') }}" 
-                onmouseout="this.src="{{ asset('img/elJuego/skin0/icons/ico-gen1.png') }}" /></a></div>
+                onmouseover=this.src="{{ asset('img/elJuego/skin0/icons/ico-gen1.png') }}" 
+                onmouseout=this.src="{{ asset('img/elJuego/skin0/icons/ico-gen0.png') }}" /></a></div>
 
 </div>
 
