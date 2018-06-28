addc41d7b8d2d7b27fc4205cbe360b6dd2a62d04
McGuten <mcguten.videos@gmail.com>
2018-06-28 23:24:49 +0200

    arregloAlmacen

--- app/Almacen.php
+++ app/Almacen.php
@@ -6,12 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Almacen extends Model
 {
+    protected $table = 'almacen';
+    protected $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nivel', 'capacidad',
+    ];
+
     public function  generarDatosAlmacenes(){
         $almacen =new Almacen();
         $almacen->nivel=1;
         $almacen->capacidad=222;
-
-        
         return $almacen;
     }
 }
