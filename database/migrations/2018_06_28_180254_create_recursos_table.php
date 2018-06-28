c3849f6be99333e9898b35f95638f1a9c499469f
McGuten <mcguten.videos@gmail.com>
2018-06-28 20:39:46 +0200

    creacionRecursos

---
+++ database/migrations/2018_06_28_180254_create_recursos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecursosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recursos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('personal');
+            $table->integer('mineral');
+            $table->integer('cristal');
+            $table->integer('gas');
+            $table->integer('plastico');
+            $table->integer('ceramica');
+            $table->integer('liquidos');
+            $table->integer('micros');
+            $table->integer('fuel');
+            $table->integer('MA');
+            $table->integer('Municion');
+            $table->integer('planeta_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recursos');
+    }
+}
