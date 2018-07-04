151c82e84a9e86e092a5ef4367e304ed1216c0d2
odinspain <odinspn@gmail.com>
2018-07-05 00:05:49 +0200

    prueba cola de construccion

--- resources/views/juego/construccion.blade.php
+++ resources/views/juego/construccion.blade.php
@@ -4,6 +4,48 @@
 <div class="container-fluid">
 <div class="container-fluid">
 
+        <div class="row rounded" style="background-image: url('http://localhost/img/elJuego/skin0/cons-fondo2.png'); border: 1px solid orange; margin-top: 5px;">
+            <div class="col-12">
+                <div id="cuadro1" class="table-responsive-sm">
+                    <table class="table table-borderless borderless table-sm text-center" >
+                        <tr>
+                            <td class=" text-warning borderless">Edificio en cola de construcción</td>
+                            <td class=" text-warning borderless">Nivel</td>
+                            <td class=" text-warning borderless">Personal</td>
+                            <td class=" text-warning borderless">Duración</td>
+                            <td class=" text-warning borderless">Acaba a las</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td class=" text-light align-middle borderless">Mina de mineral</td>
+                            <td class=" text-light align-middle borderless">Construyendo nivel 12</td>
+                            <td class=" text-light align-middle borderless">12.254.325</td>
+                            <td class=" text-light align-middle borderless">5m 33s</td>
+                            <td class=" text-light align-middle borderless">23:05:22</td>
+                            <td><button type="button" class="btn btn-outline-danger btn-block btn-sm"><i class="fa fa-trash"></i> Cancelar</button></td>
+                        </tr>
+                                                <tr>
+                            <td class=" text-light align-middle borderless">Mina de cristal</td>
+                            <td class=" text-light align-middle borderless">reciclando nivel 13</td>
+                            <td class=" text-light align-middle borderless">12.254.325</td>
+                            <td class=" text-light align-middle borderless">5m 33s</td>
+                            <td class=" text-light align-middle borderless">23:05:22</td>
+                            <td><button type="button" class="btn btn-outline-danger btn-block btn-sm"><i class="fa fa-trash"></i> Cancelar</button></td>
+                        </tr>
+                        <tr>
+                            <td class=" text-light align-middle borderless">Mina de mineral</td>
+                            <td class=" text-light align-middle borderless">reciclando nivel 13</td>
+                            <td class=" text-light align-middle borderless">12.254</td>
+                            <td class=" text-light align-middle borderless">5m 33s</td>
+                            <td class=" text-light align-middle borderless">23:10:55</td>
+                            <td><button type="button" class="btn btn-outline-danger btn-block btn-sm"><i class="fa fa-trash"></i> Cancelar</button></td>
+                        </tr>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+
     <div class="row rounded" style="background-image: url('http://localhost/img/elJuego/skin0/cons-fondo2.png'); border: 1px solid orange; margin-top: 5px;">
         <div class="col-12">
             <div id="cuadro1" class="table-responsive-sm">
@@ -12,7 +54,7 @@
                         <td colspan="4" class="text-success text-center borderless align-middle">Mina mineral nivel 30 (de 90)</td>
                         <td colspan="3" class="text-success text-center borderless align-middle">Termina: Hoy a las 20:20</td>
                         <td colspan="3" class="text-success text-center borderless align-middle">Tiempo: 0h 20m 50s</td>
-                        <td colspan="2" class="text-success text-center borderless align-middle"><input type="number" class="" placeholder="Personal"></td>
+                        <td colspan="2" class="text-success text-right borderless align-middle"><input type="number" class="personal1" placeholder="Personal" value="123456"></td>
                     </tr>
                     <tr>
                         <td rowspan="4" class="anchofijo text-warning borderless"><img class="rounded" src="http://5dim.es/eljuego/web2/skin0/fotos edif/edif1.jpg" width="90" height="90"></td>
@@ -24,11 +66,11 @@
                         <td class="anchofijo text-warning borderless">Gas</td>
                         <td class="anchofijo text-warning borderless">Plástico</td>
                         <td class="anchofijo text-warning borderless">Cerámica</td>
-                        <td class="anchofijo text-warning borderless">Liquido</td>
+                        <td class="anchofijo text-danger borderless">Liquido</td>
                         <td class="anchofijo text-warning borderless">Micros</td>
                         <td class="anchofijo text-warning borderless">Fuel</td>
-                        <td class="anchofijo text-warning borderless">M-A</td>
-                        <td class="anchofijo text-warning borderless">Munición</td>
+                        <td class="anchofijo text-muted borderless">M-A</td>
+                        <td class="anchofijo text-muted borderless">Munición</td>
                         <td class="anchofijo text-warning borderless">Personal</td>
                     </tr>
                     <tr>
@@ -37,11 +79,11 @@
                         <td class="anchofijo text-white borderless">2.222</td>
                         <td class="anchofijo text-white borderless">3.333</td>
                         <td class="anchofijo text-white borderless">444.000</td>
-                        <td class="anchofijo text-white borderless">55.050</td>
+                        <td class="anchofijo text-danger borderless">55.050</td>
                         <td class="anchofijo text-white borderless">66.006</td>
                         <td class="anchofijo text-white borderless">777.000</td>
-                        <td class="anchofijo text-white borderless">88.888</td>
-                        <td class="anchofijo text-white borderless">99.999</td>
+                        <td class="anchofijo text-muted borderless"></td>
+                        <td class="anchofijo text-mutedborderless"></td>
                         <td class="anchofijo text-white borderless">99.999</td>
                     </tr>
                     <tr>
