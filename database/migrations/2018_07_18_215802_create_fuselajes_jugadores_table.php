085136213ccdf3bdcfd44728e102b66c442b2062
McGuten <mcguten.videos@gmail.com>
2018-07-18 22:03:00 +0200

    tabla intermedia

---
+++ database/migrations/2018_07_18_215802_create_fuselajes_jugadores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuselajesJugadoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fuselajes_jugadores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fuselajes_jugadores');
+    }
+}
