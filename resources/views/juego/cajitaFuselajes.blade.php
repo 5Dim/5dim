1c552dce5607247946f89d5b9d0c77e35467d220
McGuten <mcguten.videos@gmail.com>
2018-07-20 20:05:36 +0200

    fuselajes

--- resources/views/juego/cajitaFuselajes.blade.php
+++ resources/views/juego/cajitaFuselajes.blade.php
@@ -9,18 +9,17 @@
                     </tr>
                     <tr>
                         <td rowspan="4" class="anchofijo text-warning borderless">
-                            <img class="rounded" src="{{ asset('img/fotos naves/skin1/naveMT2.jpg') }}" width="180" height="119">
+                            <img class="rounded" src="{{ asset('img/fotos naves/skin1/naveMT' . $fuselaje->id . '.jpg') }}" width="180" height="119">
                         </td>
                         <td class="anchofijo text-warning borderless">Contenedor pequeño</td>
                         <td class="anchofijo text-warning borderless">Contenedor medio</td>
                         <td class="anchofijo text-warning borderless">Contenedor grande</td>
                         <td class="anchofijo text-warning borderless">Mejoras</td>
                         <td class="anchofijo text-warning borderless">Blindaje</td>
-                        <td class="anchofijo text-warning borderless">Defensa</td>
                         <td class="anchofijo text-warning borderless">Energia</td>
-                        <td class="anchofijo text-warning borderless">Costo</td>
-                        <td class="anchofijo text-warning borderless">Mantenimiento</td>
-                        <td class="anchofijo text-warning borderless">Tiempo</td>
+                        <td class="anchofijo text-warning borderless">Cañon ligero</td>
+                        <td class="anchofijo text-warning borderless">Cañon medio</td>
+                        <td class="anchofijo text-warning borderless">Cañon pesado</td>
                     </tr>
                     <tr>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->cargaPequeña }}</td>
@@ -28,20 +27,18 @@
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->cargaGrande }}</td>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->mejoras }}</td>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->blindajes }}</td>
-                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->defensa }}</td>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->energia }}</td>
-                        <td class="anchofijo text-light borderless">0</td>
-                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->mantenimiento }}</td>
-                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->tiempo }}</td>
+                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasLigera }}</td>
+                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasMedia }}</td>
+                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasPesadas }}</td>
                     </tr>
                     <tr>
                         <td class="anchofijo text-warning borderless">Velocidad</td>
                         <td class="anchofijo text-warning borderless">Velocidad max</td>
                         <td class="anchofijo text-warning borderless">Motores</td>
                         <td class="anchofijo text-warning borderless">Fuel</td>
-                        <td class="anchofijo text-warning borderless">Cañon ligero</td>
-                        <td class="anchofijo text-warning borderless">Cañon medio</td>
-                        <td class="anchofijo text-warning borderless">Cañon pesado</td>
+                        <td class="anchofijo text-warning borderless">Mantenimiento</td>
+                        <td class="anchofijo text-warning borderless">Tiempo</td>
                         <td class="anchofijo text-warning borderless">Cañon insertado</td>
                         <td class="anchofijo text-warning borderless">Bombas</td>
                         <td class="anchofijo text-warning borderless">Misiles</td>
@@ -51,9 +48,8 @@
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->velocidadMax }}</td>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->motores }}</td>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->gastoFuel }}</td>
-                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasLigera }}</td>
-                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasMedia }}</td>
-                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasPesadas }}</td>
+                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->mantenimiento }}</td>
+                        <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->tiempo }}</td>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasInsertada }}</td>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasBombas }}</td>
                         <td class="anchofijo text-light borderless">{{ $fuselaje->cualidades->armasMisiles }}</td>
