4091f91aae3e520355e41d03e04e9d968474374c
McGuten <mcguten.videos@gmail.com>
2018-07-21 14:12:08 +0200

    cambios fuselajes

--- resources/views/juego/cajitaFuselajesPrediseñados.blade.php
+++ resources/views/juego/cajitaFuselajesPrediseñados.blade.php
@@ -3,11 +3,8 @@
             <div id="cuadro1" class="table-responsive-sm">
                 <table class="table table-borderless borderless table-sm text-center anchofijo" style="margin-top: 5px !important">
                     <tr>
-                        <td colspan="5" class="text-success text-center borderless align-middle">
+                        <td colspan="10" class="text-success text-center borderless align-middle">
                             <big>Modelo: {{ $fuselaje->codigo }}<big>
-                        </td>
-                        <td colspan="5" class="text-success text-center borderless align-middle">
-                            <big>Propiedad de {{ $fuselaje->categoria }}<big>
                         </td>
                     </tr>
                     <tr>
@@ -15,7 +12,7 @@
                             <img class="rounded" src="{{ asset('img/fotos naves/skin1/naveMT' . $fuselaje->id . '.jpg') }}" width="180" height="119">
                         </td>
                         <td  rowspan="4" colspan="9" class="anchofijo text-warning borderless">
-                            <textarea name="" id="" cols="245" rows="4" class="text-light">Lorem ipsum dolor sit amet, vivamus iaculis ligula nec enim et elit, ultricies nibh volutpat interdum erat id porttitor. Ligula at eget, ante varius nullam adipisci proin pellentesque porttitor, enim vitae. Neque in dolores ultricies consectetuer mi, nibh purus maecenas elementum suspendisse, luctus vel consequat ac erat vestibulum, turpis nulla platea hac sit, magna rutrum fringilla. Augue quis scelerisque eros orci curae, leo penatibus vestibulum, mauris enim sit ipsum, pellentesque ultrices ipsum vestibulum vestibulum viverra, luctus ex vehicula orci euismod. Sed massa lobortis, ut sed, consectetuer dictum urna vestibulum. Sociis et ut. Urna eu quos, enim tellus ut pede dolor sociosqu, laoreet sem vel eu. Fringilla risus urna amet fusce viverra, ac et risus sed, aliquet amet purus magna sed blandit bibendum.</textarea>
+                            <textarea name="" id="" rows="4" style="width: 100%" class="text-light">Lorem ipsum dolor sit amet, vivamus iaculis ligula nec enim et elit, ultricies nibh volutpat interdum erat id porttitor. Ligula at eget, ante varius nullam adipisci proin pellentesque porttitor, enim vitae. Neque in dolores ultricies consectetuer mi, nibh purus maecenas elementum suspendisse, luctus vel consequat ac erat vestibulum, turpis nulla platea hac sit, magna rutrum fringilla. Augue quis scelerisque eros orci curae, leo penatibus vestibulum, mauris enim sit ipsum, pellentesque ultrices ipsum vestibulum vestibulum viverra, luctus ex vehicula orci euismod. Sed massa lobortis, ut sed, consectetuer dictum urna vestibulum. Sociis et ut. Urna eu quos, enim tellus ut pede dolor sociosqu, laoreet sem vel eu. Fringilla risus urna amet fusce viverra, ac et risus sed, aliquet amet purus magna sed blandit bibendum.</textarea>
                         </td>
                     </tr>
                 </table>
