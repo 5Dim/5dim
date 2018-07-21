4e962af13bcf384cdea3086de2284790332a7032
odinspain <odinspn@gmail.com>
2018-07-21 20:20:40 +0200

    textos

--- resources/lang/es/investigacion.php
+++ resources/lang/es/investigacion.php
@@ -39,23 +39,23 @@ return [
     'invCargaDescripcion' => "Además de mejorar la capacidad de carga en cada nivel, en algunos podemos obtener nuevos módulos de carga para nuestras naves. Estos módulos estan agrupados por tamaños, de modo que en una nave podemos colocar en el mismo lugar carga o hangar para otras naves.",
     'invIaDescripcion' => "Podemos crear mecanismos y sistemas accesorios que mejoren las cualidades de los diseños. Por cada nivel investigado obtenemos una mejora para nuestros diseños en algún aspecto, aunque sus costes suelen ser elevados las ventajas que aportan nos premiten afinar nuestros diseños.",
 
-    'invEnsamblajeNavesDescripcion' => "Ensamblaje de naves",
-    'invEnsamblajeDefensasDescripcion' => "Ensamblaje de defensas",
-    'invEnsamblajeTropasDescripcion' => "Ensamblaje Tropas",
-    'invImperioDescripcion' => "Administración de imperio",
-    'invObservacionDescripcion' => "Observación",
+    'invEnsamblajeNavesDescripcion' => "Obtener fuselajes cada vez mejores requiere tecnologías mas avanzadas. Cada nivel de esta investigación nos da puntos para gastar en desbloquear nuevos fuselajes, estos puntos son generales de nuestra cuenta y no dependen del planeta, una vez gastados no pueden serán devueltos.",
+    'invEnsamblajeDefensasDescripcion' => "Obtener fuselajes cada vez mejores requiere tecnologías mas avanzadas. Cada nivel de esta investigación nos da puntos para gastar en desbloquear nuevos fuselajes, estos puntos son generales de nuestra cuenta y no dependen del planeta, una vez gastados no pueden serán devueltos.",
+    'invEnsamblajeTropasDescripcion' => "Obtener fuselajes cada vez mejores requiere tecnologías mas avanzadas. Cada nivel de esta investigación nos da puntos para gastar en desbloquear nuevos fuselajes, estos puntos son generales de nuestra cuenta y no dependen del planeta, una vez gastados no pueden serán devueltos.",
+    'invImperioDescripcion' => "Conforme nuestro imperio coloniza o conquista nuevos planetas necesita ampliar su estructura administrativa. Cada nivel de esta investigación nos permite tener mas planetas, si sobrepasamos esta cantidad aumentarán también los gastos diarios en moneda.",
+    'invObservacionDescripcion' => "Esta tecnología nos permite ampliar nuestra distancia de observación para cada nivel de edificio de observatorio en cada planeta. Igualmente hace mas dificil que puedan ver lo que contiene nuestro planeta en la misma proporción. Igualmente para las naves que pasen en vuelo por nuestra área de observación veremos mas detalles según el nivel de esta tecnología, el nivel de edificio y la distancia a la que se encuentre. Los miembros de una alianza comparten el edificio pero se aplica la tecnología de cada uno.",
 
-    'invPropNukDescripcion' => "Propulsión nuclear",
-    'invPropIonDescripcion' => "Propulsión iónica",
-    'invPropPlasmaDescripcion' => "Propulsión de plasma",
-    'invPropMaDescripcion' => "Propulsión antimateria",
-    'invPropHMADescripcion' => "Propulsión HMA",
+    'invPropNukDescripcion' => "Estos motores son grandes y no dan mucha velocidad, aunque dan una muy buena cantidad de energía pese a un precio muy bajo.",
+    'invPropIonDescripcion' => "Los nuevos avances tecnoloógicos nos premiten crear un tipo de motor que de mas velocidad y menos gasto como alternativa al motor nuclear. Sin embargo este motor aporta menos energía y resta mas defensa.",
+    'invPropPlasmaDescripcion' => "Estos motores aportan una cantidad enorme de energía y velocidad, el problema es que resta mucha defensa y es caro de producir.",
+    'invPropMaDescripcion' => "El precio de esta tecnología se amortiza con las porpiedades de estos motores, ya que aportan una cantidad buena de energía a bajo precio, por otro lado restan mucha defensa, su velocidad no es tan alta como cabría esperar y el tiempo de producción es largo.",
+    'invPropHMADescripcion' => "Mas allá de la optimización y aumento de un motor de animatería es posible crear una burbuja de disorsión dentro de la cual los objetos acceden a otra región conocida del universo, la maquina necesaria se le denomina motor HMA.<br> Este motor sólo puede montarse en las naves nodriza y tiene usos especiales además de a la propia propulsión, tales como desplazar flotas enteras al instante o disparar un haz de energía a flotas en vuelo y planetas.",
 
-    'invIndLiquidoDescripcion' => "Mejora industria de liquidos",
-    'invIndMicrosDescripcion' => "Mejora industria de micros",
-    'invIndFuelDescripcion' => "Mejora industria de fuél",
-    'invIndMaDescripcion' => "Mejora industria  de MA",
-    'invIndMunicionDescripcion' => "Mejora industria  de munición",
+    'invIndLiquidoDescripcion' => " Con los conocimientos adquiridos en nuestras fábricas de producción de líquidos podemos mejorar sus sistemas de refinado y destilado, obteniendo catalizadores y sistemas que nos aumenten considerablemente la producción.",
+    'invIndMicrosDescripcion' => "Con la misma tecnología que usamos para crear nuestras naves, podemos crear familias de nanobots que se encargen de crear los componentes electrónicos necesarios para nuestras necesidades. Así podemos crear cada vez mas y mejores circuitos a un precio menor.",
+    'invIndFuelDescripcion' => "Hemos estado produciendo tal cantidad de combustible para nuestras naves que nos hemos podido expandir por nuestros alrededores y mejorar nuestro forma de relacionarnos con nuestra especie y con otras. Intercambiando conocimientos hemos averiguado que existen otros combustibles y que podemos producir más y mejor de los que ya conocíamos.",
+    'invIndMaDescripcion' => "Nuestros avances en el campo del manejo de la antimateria nos permiten crear una rama de la tecnología que se encargue exclusivamente en optimizar los metodos de su producción quedando al margen de los avances científicos en su aprovechamiento.",
+    'invIndMunicionDescripcion' => "Si queremos mantener una armada operativa necesitamos primero de la suficiente cantidad de munición que asegure su funcionamiento en épocas de mayor necesidad, así podemos desarrollar sistemas productivos que mejores el funcionamiento de nuestras fábricas de munición.",
 
 
 
