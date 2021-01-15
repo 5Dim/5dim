2aa34f490daf226f45bf1cdf373aeb75f50246bd
McGuten <mcguten.videos@gmail.com>
2021-01-15 10:06:44 +0100

    delete SQL

--- construcciones.sql
+++
@@ -1,4 +0,0 @@
-update investigaciones set nivel=20;
-UPDATE construcciones SET nivel = 30;
-UPDATE construcciones SET nivel = 12 WHERE codigo LIKE 'ind%';
-UPDATE industrias SET liquido = 1, micros = 1, fuel = 1, ma = 1, municion = 1;
