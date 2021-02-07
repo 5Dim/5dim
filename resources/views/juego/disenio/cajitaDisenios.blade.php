1152fdcc7a728c0436e0763a30eba4d2b9af97df
odinspain <odinspn@gmail.com>
2021-02-07 19:04:55 +0100

    calculo horas tiempo

--- resources/views/juego/disenio/cajitaDisenios.blade.php
+++ resources/views/juego/disenio/cajitaDisenios.blade.php
@@ -158,7 +158,7 @@
                             </td>
                     @endif
                     <td id="tiempo{{ $disenio->id }}" class="anchofijo text-light borderless">
-                        {{ gmdate('d:H:i:s',round($disenio->mejoras->tiempo /(1+($constanteVelocidad * $nivelHangar/100)))) }}
+                        {{round(($disenio->mejoras->tiempo /(1+($constanteVelocidad * $nivelHangar/100)))/3600)}}:{{gmdate('i:s',round($disenio->mejoras->tiempo /(1+($constanteVelocidad * $nivelHangar/100)))) }}
                     </td>
                 </tr>
                 <tr>
