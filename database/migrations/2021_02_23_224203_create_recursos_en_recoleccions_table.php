0f0c373d51a4b2c71065a7fa9624d5e4c438b7d9
odinspain <odinspn@gmail.com>
2021-04-02 18:44:56 +0200

    cambios vision flotas en vuelo

--- database/migrations/2021_02_23_224203_create_recursos_en_recoleccions_table.php
+++ database/migrations/2021_02_23_224203_create_recursos_en_recoleccions_table.php
@@ -15,18 +15,18 @@ class CreateRecursosEnRecoleccionsTable extends Migration
     {
         Schema::create('recursos_en_recoleccions', function (Blueprint $table) {
             $table->id();
-            $table->decimal('personal', 13, 2)->default(0); //99.999.999.999
-            $table->decimal('mineral', 13, 2)->default(0);
-            $table->decimal('cristal', 13, 2)->default(0);
-            $table->decimal('gas', 11, 2)->default(0);
-            $table->decimal('plastico', 11, 2)->default(0);
-            $table->decimal('ceramica', 11, 2)->default(0);
-            $table->decimal('liquido', 11, 2)->default(0);
-            $table->decimal('micros', 11, 2)->default(0);
-            $table->decimal('fuel', 11, 2)->default(0);
-            $table->decimal('ma', 11, 2)->default(0);
-            $table->decimal('municion', 11, 2)->default(0);
-            $table->decimal('creditos', 13, 2)->default(0);
+            $table->unsignedBigInteger('personal')->default(0);
+            $table->unsignedBigInteger('mineral')->default(0);
+            $table->unsignedBigInteger('cristal')->default(0);
+            $table->unsignedBigInteger('gas')->default(0);
+            $table->unsignedBigInteger('plastico')->default(0);
+            $table->unsignedBigInteger('ceramica')->default(0);
+            $table->unsignedBigInteger('liquido')->default(0);
+            $table->unsignedBigInteger('micros')->default(0);
+            $table->unsignedBigInteger('fuel')->default(0);
+            $table->unsignedBigInteger('ma')->default(0);
+            $table->unsignedBigInteger('municion')->default(0);
+            $table->unsignedBigInteger('creditos')->default(0);
             $table->unsignedBigInteger('enrecoleccions_id')->unsigned();
             $table->foreign('enrecoleccions_id')->references('id')->on('en_recoleccions');
             $table->timestamps();
--- database/migrations/2021_02_23_225430_create_en_recursos_en_destinos_table.php
+++ database/migrations/2021_02_23_225430_create_en_recursos_en_destinos_table.php
@@ -15,18 +15,18 @@ class CreateEnRecursosEnDestinosTable extends Migration
     {
         Schema::create('en_recursos_en_destinos', function (Blueprint $table) {
             $table->id();
-            $table->decimal('personal', 13, 2)->default(0); //99.999.999.999
-            $table->decimal('mineral', 13, 2)->default(0);
-            $table->decimal('cristal', 13, 2)->default(0);
-            $table->decimal('gas', 11, 2)->default(0);
-            $table->decimal('plastico', 11, 2)->default(0);
-            $table->decimal('ceramica', 11, 2)->default(0);
-            $table->decimal('liquido', 11, 2)->default(0);
-            $table->decimal('micros', 11, 2)->default(0);
-            $table->decimal('fuel', 11, 2)->default(0);
-            $table->decimal('ma', 11, 2)->default(0);
-            $table->decimal('municion', 11, 2)->default(0);
-            $table->decimal('creditos', 13, 2)->default(0);
+            $table->unsignedBigInteger('personal')->default(0);
+            $table->unsignedBigInteger('mineral')->default(0);
+            $table->unsignedBigInteger('cristal')->default(0);
+            $table->unsignedBigInteger('gas')->default(0);
+            $table->unsignedBigInteger('plastico')->default(0);
+            $table->unsignedBigInteger('ceramica')->default(0);
+            $table->unsignedBigInteger('liquido')->default(0);
+            $table->unsignedBigInteger('micros')->default(0);
+            $table->unsignedBigInteger('fuel')->default(0);
+            $table->unsignedBigInteger('ma')->default(0);
+            $table->unsignedBigInteger('municion')->default(0);
+            $table->unsignedBigInteger('creditos')->default(0);
             $table->unsignedBigInteger('destinos_id')->unsigned();
             $table->foreign('destinos_id')->references('id')->on('destinos');
             $table->timestamps();
