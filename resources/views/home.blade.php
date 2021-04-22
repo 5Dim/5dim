36243ef15d7460c2902d4c7fc68215f77649df5f
McGuten <mcguten.videos@gmail.com>
2021-04-22 11:32:43 +0200

    cambio a la pag ppal y update de bootstrap b1 a b3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,9 @@
-@extends('layouts.app')
+@extends('principal.layout')
 
 @section('content')
     <div class="container">
         <div class="alert alert-dark" role="alert" style="margin-top: 100px">
-            <h4 class="alert-heading text-center">Universo principal</h4>
-            <p>
-                Este universo tiene los parametros estandar
-            </p>
-            <hr>
+            <h2 class="text-center fs-1">Ronda 2022</h2>
             <table class="table table-striped table-secondary table-hover table-sm">
                 <thead>
                     <tr>
@@ -26,52 +22,9 @@
                     </tr>
                 </tbody>
             </table>
-            <hr>
             <p class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button type="button" class="btn btn-dark me-md-2" onclick="location.href='{{ url('/juego/construccion') }}'">
-                    Entrar
-                </button>
-            </p>
-        </div>
-        <div class="alert alert-dark" role="alert">
-            <h4 class="alert-heading text-center">Universo Mutante</h4>
-            <p>
-                Este universo tiene los parametros alterados
-            </p>
-            <hr>
-            <table class="table table-striped table-secondary table-hover table-sm">
-                <thead>
-                    <tr>
-                        <th scope="col">Opcion</th>
-                        <th scope="col">Configuracion</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">Velocidad construccion</th>
-                        <td>5</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Velocidad investigacion</th>
-                        <td>5</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Velocidad fabricas</th>
-                        <td>15</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Creacion del universo</th>
-                        <td>01/02/2019</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Fecha de finalizacion del universo (prevista)</th>
-                        <td>28/02/2020</td>
-                    </tr>
-                </tbody>
-            </table>
-            <hr>
-            <p class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button type="button" class="btn btn-dark me-md-2" onclick="location.href='{{ url('/jugador/1') }}';">
+                <button type="button" class="btn btn-dark me-md-2"
+                    onclick="location.href='{{ url('/juego/construccion') }}'">
                     Entrar
                 </button>
             </p>
---
+++ resources/views/principal/layout.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Quinta Dimension</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+
+    <!-- Icons Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+        integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+        crossorigin="anonymous" />
+
+    <!-- Styles -->
+    <link href="{{ asset('css/bootstrap/bootstrap.min.css') }}" media="all" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/customPpal.css') }}" rel="stylesheet">
+
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/bootstrap/carousel.css') }}" media="all" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="bg text-center">
+    <div id="app">
+        <nav class="navbar navbar-expand-md">
+            <div class="container">
+                <a class="navbar-brand text-white" href="{{ url('/') }}">
+                    <img src="{{ asset('favicon.png') }}" alt="" width="30" height="24"
+                        class="d-inline-block align-text-top">
+                    Quinta Dimension
+                </a>
+                @auth
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </button>
+
+                        <div class="dropdown-menu bg-dark" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item bg-dark text-light" href="{{ url('/home') }}">Acceder a los universos</a>
+                            <a class="dropdown-item bg-dark text-light" href="{{ url('/configuracion') }}">Configuracion</a>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a class="dropdown-item bg-danger text-light" href="{{ url('/logout') }}"
+                                    onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
+                            </form>
+                        </div>
+                    </div>
+                @endauth
+                @guest
+                    <div class="col-md-3 text-end">
+                        <a class="" href="{{ url('/login') }}"><button type="button"
+                                class="btn btn-primary me-2">Login</button></a>
+                        <a class="" href="{{ url('/register') }}"><button type="button"
+                                class="btn btn-primary">Registrar</button></a>
+                    </div>
+                @endguest
+            </div>
+        </nav>
+    </div>
+    <main class="text-white">
+        @yield('content')
+    </main>
+    </div>
+
+    <!-- Bootstrap -->
+    <script src="{{ asset('js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+
+</html>
