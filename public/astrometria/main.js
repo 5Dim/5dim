e01c7388a45b65b17d0471ef4919cd42a1f1a729
McGuten <mcguten.videos@gmail.com>
2021-05-25 13:14:32 +0200

    flotas

--- public/astrometria/main.js
+++ public/astrometria/main.js
@@ -405,8 +405,6 @@ function createViewport() {
             /////////////////////////////////////////////////
         });
 
-    cargaTexturasGeneral(); // llamo a la carga de las animaciones
-
     barra1 = app.stage.addChild(new PIXI.Container()); // crea la capa botones
     auxImg = app.stage.addChild(new PIXI.Container()); // contenedor para dibujos temporales
     botones = app.stage.addChild(new PIXI.Container()); // crea la capa botones
@@ -427,6 +425,9 @@ function createViewport() {
 
     barraCarga = app.stage.addChild(new PIXI.Graphics());
     barraCarga.position.set(0, 0);
+
+    cargaTexturasGeneral(); // llamo a la carga de las animaciones
+    cargaCompleta();
 
     /*
         Shockwave_Filter = new PIXI.filters.ShockwaveFilter();
