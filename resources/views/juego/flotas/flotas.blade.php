b7863a1fbdb16adf068c53c2a0a20e31b6284e77
McGuten <mcguten.videos@gmail.com>
2021-06-01 09:11:50 +0200

    tab flotas

--- resources/views/juego/flotas/flotas.blade.php
+++ resources/views/juego/flotas/flotas.blade.php
@@ -6,11 +6,11 @@
             <nav>
                 <div class="nav nav-pills nav-justified" id="nav-tab" role="tablist" style="border: 0px; margin: 5px"
                     align="center">
-                    <a class="nav-item nav-link " id="enviar-tab" data-bs-toggle="tab" href="#enviar" role="tab"
+                    <a class="nav-item nav-link" id="enviar-tab" data-bs-toggle="tab" href="#enviar" role="tab"
                         aria-controls="Enviar" aria-selected="true">
                         Enviar
                     </a>
-                    <a class="nav-item nav-link active" id="envuelo-tab" data-bs-toggle="tab" href="#envuelo" role="tab"
+                    <a class="nav-item nav-link" id="envuelo-tab" data-bs-toggle="tab" href="#envuelo" role="tab"
                         aria-controls="envuelo" aria-selected="false" onclick="verFlotasEnVuelo()">
                         En vuelo
                     </a>
@@ -33,7 +33,7 @@
                 <div class="tab-pane fade " id="enviar" role="tabpanel" aria-labelledby="enviar-tab">
                     @include('juego.flotas.enviar')
                 </div>
-                <div class="tab-pane fade show active" id="envuelo" role="tabpanel" aria-labelledby="envuelo-tab">
+                <div class="tab-pane fade" id="envuelo" role="tabpanel" aria-labelledby="envuelo-tab">
                     @include('juego.flotas.cajitaFlotas', [
                         'envuelo' => true
                         ])
