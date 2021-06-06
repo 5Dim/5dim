f646fac4f1f150dd7a1e260c9564c73bde3229fe
McGuten <mcguten.videos@gmail.com>
2021-06-06 17:27:15 +0200

    dismiss cookie alert

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -25,21 +25,23 @@
     }
 
 </style>
-<div class="row">
-    <div class="col-md-4 col-sm-12 button-fixed">
-        <div class="p-3 pb-4 bg-custom text-white">
-            <div class="row">
-                <div class="col-12">
-                    <span class="cookie-consent__message">
+<div class="row js-cookie-consent cookie-consent fixed bottom-0 inset-x-0 pb-2">
+    <div class="col-md-4 col-sm-12 button-fixed max-w-7xl mx-auto px-6">
+        <div class="p-3 pb-4 bg-custom text-white p-2 rounded-lg bg-yellow-100">
+            <div class="row flex items-center justify-between flex-wrap">
+                <div class="col-12 ml-3 text-yellow-600 cookie-consent__message">
+                    <span class="cookie-consent__message ml-3 text-yellow-600 cookie-consent__message">
                         {!! trans('cookie-consent::texts.message') !!}
                     </span>
                 </div>
             </div>
-            <p>
-            </p>
-            <button type="button" class="btn btn-success w-100 js-cookie-consent-agree cookie-consent__agree">
-                {{ trans('cookie-consent::texts.agree') }}
-            </button>
+            <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
+                <a type="button"
+                    class="btn btn-success js-cookie-consent-agree cookie-consent__agree flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-yellow-800 bg-yellow-400 hover:bg-yellow-300">
+                    {{ trans('cookie-consent::texts.agree') }}
+                </a>
+
+            </div>
         </div>
     </div>
 </div>
