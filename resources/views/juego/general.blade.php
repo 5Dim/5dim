bd368c4afe4f3644a3e98418ef6f382d6256c5c6
odinspain <odinspn@gmail.com>
2021-07-13 00:43:38 +0200

    minicambios

--- resources/views/juego/general.blade.php
+++ resources/views/juego/general.blade.php
@@ -25,9 +25,13 @@
                         </div>
                     @else
                         <div class="alert alert-success cajita-success text-light" role="alert">
-                            <h4 class="alert-heading">Bienvenidos a una version en desarrollo de 5Dim.es</h4>
+                            <h4 class="alert-heading">Bienvenidos a una version V0.5 de 5Dim.es</h4>
                             <p> Ahí van unos consejos de inicio:</p>
                             <ul>
+                                <li>
+                                    Si acabas de entrar y no te gusta tu posición de inicio,<span class="text-warning"> puedes mover  tu colonia </span>
+                                    desde el menu de planeta (una sola vez), poniendo una posición vacía del mapa.
+                                </li>
                                 <li>
                                     Todo lo relacionado con combates NO está implementado, de modo que no investigues ni
                                     diseñes con armas o blindajes.
@@ -36,15 +40,15 @@
                                     Céntrate al inicio en subir las minas de tu planeta inicial, priorizando el mineral.
                                 </li>
                                 <li>
-                                    Construir una nave y mandarla a recolectar en los asteroides que tienes al lado de tu
-                                    planeta es una buena idea una vez puedas permitirte construir una nave.
+                                    Construir una nave con recolección y mandarla a recolectar en los asteroides que tienes al lado de tu
+                                    planeta es una buena idea una vez puedas permitirte construirla.
                                 </li>
                                 <li>
                                     Colonizar planetas cercanos es una buena opción, puedes hacerlo incluso con una sonda
                                     ya que es muy barata.
                                 </li>
                                 <li>
-                                    Además puedes mandar tu sonda (con combustible) a orbitar otras estrellas fuera de tu
+                                    Además puedes mandar una sonda (con combustible) a orbitar otras estrellas fuera de tu
                                     visión, ya que también da algo de visión.
                                 </li>
                                 <li>
@@ -52,7 +56,7 @@
                                     comparten de forma automática.
                                 </li>
                                 <li>
-                                    Además las naves se mejoran automaticamente cada vez que subes una tecnología.
+                                    Las naves se mejoran automaticamente cada vez que subes una tecnología.
                                 </li>
                                 <li>
                                     Subir la tecnología de fuselajes es siempre muy rentable, pero ten en cuenta que las
