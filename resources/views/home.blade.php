806e4abdd3b5877ac440cb20779a86bfbf4657a4
odinspain <odinspn@gmail.com>
2021-07-12 23:03:37 +0200

    mensaje ronda 05

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="alert alert-dark" role="alert" style="margin-top: 100px">
-            <h2 class="text-center fs-1">Ronda 2022</h2>
+            <h2 class="text-center fs-1">Ronda 2021 V0.5</h2>
             <hr>
             <table class="table table-dark">
                 <thead>
@@ -15,33 +15,38 @@
                 <tbody>
                     <tr>
                         <th scope="row">Creación del universo</th>
-                        <td>01/01/2022</td>
+                        <td>16/07/2021</td>
                     </tr>
                     <tr>
                         <th scope="row">Fecha de finalización del universo (prevista)</th>
-                        <td>30/12/2022</td>
+                        <td>30/12/2021</td>
                     </tr>
                 </tbody>
             </table>
             <hr>
             <p class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <span class="text-danger borderless">  </span>
+
                 <button type="button" class="btn btn-dark me-md-2" onclick="location.href='{{ url('/juego/construccion') }}'">
-                    Entrar
+                    Entrar (Cierre ronda actual el miércoles 14-7-2021, apertura viernes 16-7-2021)
                 </button>
             </p>
         </div>
-        <div class="alert alert-success" role="alert">
-            <h4 class="alert-heading">Disponible en la versión actual alfa V0.1</h4>
+        <div class="alert alert-success" role="alert"  style="text-align: left !important;">
+            <h4 class="alert-heading">Cambios en la versión alfa V0.5</h4>
             <ul >
-                <li >Construcción en planetas.</li>
-                <li>Investigaciones.</li>
-                <li>Diseño y construcción de naves.</li>
-                <li>Alianzas.</li>
-                <li>Mapa Astrometría dinámico.</li>
-                <li>Movimientos de flotas con todas las misiones menos combate.</li>
+                <li>Alianzas: ya no disponen de jugador ni planetas.</li>
+                <li>Estadísticas: ahora se calculan por recursos gastados, los recursos mas valiosos dan mas puntos.</li>
+                <li>Investigaciones: cuestan mas cuanto mas miembros tienen.</li>
+                <li>Politicas: Modifican parámetros de juego, de lunes a miercoles se proponen politicas, de miercoles a domingo se votan, el domingo toman efecto. Los jugadores con mas puntos disponen de mas votos.</li>
+                <li>Puntos de victoria: Se ganan por permanecer arriba en las estadísticas, se actualizan cada 1h.</li>
+                <li>Interface: nueva interface.</li>
+                <li>Rutas predefinidas: Se pueden crear rutas predefinidas para nuestras flotas.</li>
+                <li>Universo: generación de universo con mas objetos.</li>
+                <li>Bugfixes: correcciones varias y mejoras de optimización en los cálculos</li>
             </ul>
         </div>
-        <div class="alert alert-warning" role="alert">
+        <div class="alert alert-warning" role="alert"  style="text-align: left !important;">
             <h4 class="alert-heading">Para la siguiente versión beta V0.9</h4>
             <ul>
                 <li >Todo lo relacionado con combates.</li>
@@ -52,7 +57,7 @@
                 <li>Personalización del usuario.</li>
             </ul>
         </div>
-        <div class="alert alert-danger" role="alert">
+        <div class="alert alert-danger" role="alert"  style="text-align: left !important;">
             <h4 class="alert-heading">Versión final V1.0</h4>
             <ul>
                 <li >Piratas gestionados por la IA.</li>
